770a0dd6d3539cbc7a32db0293704c9c2c40ffdc
c2clt <jmzh119yjm@gmail.com>
2021-04-22 11:36:22 -0400

    Queue structure by deque

--- 05_02_begin/queue_ll.py
+++ 05_02_begin/queue_ll.py
@@ -8,4 +8,44 @@ from collections import deque
 
 
 class Queue:
-    pass
+    def __init__(self):
+        self.items = deque()
+
+    def is_empty(self):
+        return not self.items
+        # return len(self.items) == 0
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        return self.items.popleft()
+
+    def size(self):
+        return len(self.items)
+
+    def peek(self):
+        return self.items[0]
+
+    def __str__(self):
+        return str(self.items)
+
+if __name__ == '__main__':
+    q = Queue()
+    q.enqueue('Learning')
+    q.enqueue('With')
+    q.dequeue()
+    q.enqueue('Linked')
+    q.enqueue('In') 
+    q.dequeue()
+    print(q)
+    # print(q.is_empty())
+    # q.enqueue('A')
+    # q.enqueue('B')
+    # q.enqueue('G')
+    # print(q)
+    # print(q.dequeue())
+    # print(q)
+    # print(q.size())
+    # print(q.peek())
+    # print(q)
--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Python_DSA
 
 1. build Stack structure by List and reverse string by Stack
+   
    is_empty(), push(item), pop(), peek(), size()
+   
 2. 2D list: Maze
+   
    -- read Maze from .txt file
